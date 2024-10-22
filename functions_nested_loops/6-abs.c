0f10144e240e3af607e0c13f6d59e7c467b5acde
MrVugar <vuqarazizov7777@gmail.com>
2024-10-22 06:19:46 +0000

    task6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-	* _abs - computes the absolute value of an integer
-	* @n: the integer to be computed
-	*
-	* Return: the absolute value of the integer
-	*/
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to compute the absolute value for.
+ *
+ * Return: The absolute value of the integer.
+ */
 int _abs(int n)
 {
-		if (n < 0)
-		{
-		return (-n);
-		}
-		else
-		{
-			return (n);
-		}
+    if (n < 0)
+    {
+        return (-n); /* Əgər n mənfidirsə, onun müsbət dəyərini qaytar */
+    }
+    return (n); /* Əks halda, n-in dəyərini qaytar */
 }
--- functions_nested_loops/6-main.c
+++ functions_nested_loops/6-main.c
@@ -2,22 +2,21 @@
 #include <stdio.h>
 
 /**
-	* main - check the code
-	*
-	* Return: Always 0.
-	*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int r;
+    int r;
 
-		r = _abs(-1);
-    printf("%d\n", r);
+    r = _abs(-1);
+    printf("%d\n", r); /* 1 */
     r = _abs(0);
-    printf("%d\n", r);
+    printf("%d\n", r); /* 0 */
     r = _abs(1);
-    printf("%d\n", r);
+    printf("%d\n", r); /* 1 */
     r = _abs(-98);
-    printf("%d\n", r);
+    printf("%d\n", r); /* 98 */
     return (0);
 }
-
