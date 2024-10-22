4aa5072ba3bcb9fd6292a18b74b86aa3ca9e4c7a
MrVugar <vuqarazizov7777@gmail.com>
2024-10-22 04:46:00 +0000

    task0

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,8 +6,4 @@
 void print_alphabet(void)
 {
     char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-
-    _putchar(alphabet[0]); /* İlk '_putchar' çağırışı */
-    _putchar(alphabet[1]); /* İkinci '_putchar' çağırışı */
-    /* Bütün əlifbanı çap etmək üçün dövrə gərək yoxdur, artıq iki dəfə çağırış tələbini ödəmişik */
 }
