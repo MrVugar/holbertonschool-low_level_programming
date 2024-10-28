fd6bdf783fcba7dd2a784b8d6ea02f146859e620
MrVugar <vuqarazizov7777@gmail.com>
2024-10-28 18:45:34 +0000

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
-/**
- * puts2 - stringin hər ikinci simvolunu çap edir
- * @str: çap olunacaq stringin işarəçisi
- *
- * Return: void
- */
 void puts2(char *str)
 {
-	int i;
+    int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (i % 2 == 0)
+        {
+            _putchar(str[i]);
+        }
+    }
+    _putchar('\n');
 }
