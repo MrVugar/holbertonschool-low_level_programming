2bb9f9ec8308aaa3b6539a9872ef0c11f091c43e
MrVugar <vuqarazizov7777@gmail.com>
2024-10-28 19:04:11 +0000

    task6

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,15 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n - 1)  /* Check to avoid trailing comma */
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");  /* New line after printing the array */
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)  /* Check to avoid trailing comma */
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");  /* New line after printing the array */
 }
