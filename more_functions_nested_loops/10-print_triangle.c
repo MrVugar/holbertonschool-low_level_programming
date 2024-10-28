6bfcb7862e8f462007dbe946bbcdfc41b1dfbefe
MrVugar <vuqarazizov7777@gmail.com>
2024-10-28 04:57:26 +0000

    task10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,24 +2,25 @@
 
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i, j, k;
-        for (i = 1; i <= size; i++)
-        {
-            for (j = size - i; j > 0; j--)
-            {
-                _putchar(' ');
-            }
-            for (k = 1; k <= i; k++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j, k;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 
+/**
+ * main - 1-dən 100-ə qədər FizzBuzz loqikasını çap edir.
+ *
+ * Return: Həmişə 0.
+ */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
 
-        if (i < 100)
-        {
-            printf(" ");
-        }
-    }
-    printf("\n");
-    return (0);
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
+
