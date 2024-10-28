e5363d7a894c29f6a84919dafd141515ee17596f
MrVugar <vuqarazizov7777@gmail.com>
2024-10-28 04:43:19 +0000

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        int i, j, k;
+        for (i = 1; i <= size; i++)
+        {
+            for (j = size - i; j > 0; j--)
+            {
+                _putchar(' ');
+            }
+            for (k = 1; k <= i; k++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
