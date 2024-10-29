68944acd4e4f096b7ffac1664d4b0790d39a6ef3
MrVugar <vuqarazizov7777@gmail.com>
2024-10-29 04:52:09 +0000

    task1

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -2,17 +2,47 @@
 #include <stdio.h>
 
 /**
- * main - kodu yoxlayır
+ * main - Kodu yoxlayır
  *
- * Return: Həmişə 0.
+ * Return: Həmişə 0 qaytarır
  */
 int main(void)
 {
-    char *str;
-    int len;
+	char s1[98];
+	char *ptr;
+	int i;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+	/* s1 massivinin hər bir elementini * simvolu ilə doldururuq */
+	for (i = 0; i < 98 - 1; i++)
+	{
+		s1[i] = '*';
+	}
+	s1[i] = '\0';
+	printf("%s\n", s1);
+
+	/* s1-ə 5 simvol kopyalayırıq */
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+
+	/* s1-ə bütün mənbə sətirini kopyalayırıq */
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", ptr);
+
+	/* s1 massivinin hər bir elementini hex şəklində çap edirik */
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
 }
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - Bir sətirdən digərinə maksimum n simvol kopyalayır
+ * @dest: Kopyalama üçün təyinat sətiri
+ * @src: Kopyalanacaq mənbə sətiri
+ * @n: Mənbə sətirindən maksimum kopyalanacaq simvol sayı
+ *
+ * Return: Yekun sətirin göstəricisi (dest)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	/* src sətirini dest-ə maksimum n simvol qədər kopyalayırıq */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	/* Əgər src n-dən az simvola malikdirsə, qalan yerləri null ilə doldururuq */
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
