720a42aed74d38e874a65ea36a8223729bc4b545
MrVugar <vuqarazizov7777@gmail.com>
2024-10-29 05:37:23 +0000

    task1

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,20 @@
  */
 char *leet(char *str)
 {
-    int i, j;
-    char letters[] = "aeotlAEOTL";
-    char replacements[] = "4307143071";
+	int i, j;
+	char letters[] = "aeotlAEOTL";
+	char replacements[] = "4307143071";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; j < 10; j++)  /* 10 s[] and replacements[] */
-        {
-            if (str[i] == letters[j])
-            {
-                str[i] = replacements[j];
-            }
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++) /* 10 is the lengtents[] */
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = replacements[j];
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
