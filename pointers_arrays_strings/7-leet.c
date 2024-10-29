a795053deef5e5f47f32339fa618375aa2460f57
MrVugar <vuqarazizov7777@gmail.com>
2024-10-29 05:35:35 +0000

    task1

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+    int i, j;
+    char letters[] = "aeotlAEOTL";
+    char replacements[] = "4307143071";
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; j < 10; j++)  /* 10 s[] and replacements[] */
+        {
+            if (str[i] == letters[j])
+            {
+                str[i] = replacements[j];
+            }
+        }
+    }
+
+    return (str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,6 +20,7 @@ void reverse_array(int *a, int n);
 void print_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
+char *leet(char *);
 int _putchar(char c);
 
 #endif /* MAIN_H */
