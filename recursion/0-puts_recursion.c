efb0d42fd7fd818dc3bd11c473877dca3f3f0fdc
MrVugar <vuqarazizov7777@gmail.com>
2024-11-04 04:54:34 +0000

    task0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,14 +3,16 @@
 /**
  * _puts_recursion - Sətiri çap edir və yeni sətrə keçir.
  * @s: Çap ediləcək sətir.
+ *
+ * Return: Heç nə qaytarmır.
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');  /* Əgər sətirin sonuna çatmışıqsa, yeni sətir çap edirik */
-        return;
-    }
-    _putchar(*s);         /* Cari xarakteri çap edir */
-    _puts_recursion(s + 1); /* Növbəti xarakter üçün funksiyanı rekursiv olaraq çağırır */
+	if (*s == '\0')
+	{
+		_putchar('\n'); /* Sətirin sonuna çatıldıqda yeni sətrə keçir */
+		return;
+	}
+	_putchar(*s);        /* Cari xarakteri çap edir */
+	_puts_recursion(s + 1); /* Növbəti xarakter üçün funksiyanı rekursiv olaraq çağırır */
 }
