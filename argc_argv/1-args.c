3614c1923425e3d10aa70a35bc77c63a9a26e254
MrVugar <vuqarazizov7777@gmail.com>
2024-11-05 05:09:50 +0000

    task

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints the number of arguments passed into the program
- * @argc: Argument count
- * @argv: Array of arguments (unused)
- *
- * Return: Always 0
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
  */
+
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-    printf("%d\n", argc - 1);
-    return (0);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
 }
