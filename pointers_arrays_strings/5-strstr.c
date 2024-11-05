a0db18e403d72d1a352a4745c7290ce67a473a7d
MrVugar <vuqarazizov7777@gmail.com>
2024-11-05 05:30:19 +0000

    task

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,41 +1,40 @@
-#include <stddef.h>
 #include "main.h"
+#define NULL 0
 
 /**
- * _strstr - Locates a substring in a string
- * @haystack: The string to be searched
- * @needle: The substring to locate
- *
- * Return: A pointer to the beginning of the located substring,
- *         or NULL if the substring is not found
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-    char *h;
-    char *n;
+	int i = 0, j, x;
 
-    if (*needle == '\0')
-    {
-        return (haystack);
-    }
+	if (needle[0] == '\0')
+		return (haystack);
 
-    while (*haystack)
-    {
-        h = haystack;
-        n = needle;
-
-        while (*n && (*h == *n))
-        {
-            h++;
-            n++;
-        }
-
-        if (!*n)
-        {
-            return (haystack);
-        }
-
-        haystack++;
-    }
-    return (NULL);
+	while (haystack[i] != '\0') /* iterate through haystack */
+	{
+		/* if a byte matches first char of needle */
+		/* interate through needle until match ends */
+		if (haystack[i] == needle[0])
+		{
+			x = i, j = 0;
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			} /* if matched throughout, return haystack */
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return (NULL); /* No match */
 }
--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,17 +1,31 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print sums of diagonals in matrix
+ * @a: matrix
+ * @size: size of matrix
+ */
 
 void print_diagsums(int *a, int size)
 {
-    int i;
-    int sum1 = 0;
-    int sum2 = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum1 += a[i * size + i];
-        sum2 += a[i * size + (size - 1 - i)];
-    }
+	int diagonal_sum_1 = 0;
+	int diagonal_sum_2 = 0;
+	int row, i;
 
-    printf("%d, %d\n", sum1, sum2);
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		diagonal_sum_1 += a[i];
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		diagonal_sum_2 += a[i];
+	}
+
+	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
+
 }
